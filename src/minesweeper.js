fe4176f709704c0b581bcb1287102ec638d2dfa4
Martin Greenaway <martin@four-oh-four.co.uk>
2017-09-12 00:13:02 +0100

    Updated for Day 10 and 11

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,53 @@
-// function to print out the board in a nice format
-const printBoard = (board) => {
-	console.log('Current Board:');
-	console.log(board[0].join(' | '));
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	let board = [];
+
+	for(let r = 0; r < numberOfRows; r++) {
+		let row = [];
+
+		for(let c = 0; c < numberOfColumns; c++) {
+			row.push(' ');
+		}
+
+		board.push(row);
+	}
+
+	return board;
 }
 
-// Create empty board
-let board = [[' ', ' ', ' '],
-             [' ', ' ', ' '],
-             [' ', ' ', ' ']];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
 
-// Actually display the board
-printBoard(board);
+	for(let r = 0; r < numberOfRows; r++) {
+		let row = [];
 
-board[0][1] = '1';
-board[1][2] = 'B';
+		for(let c = 0; c < numberOfColumns; c++) {
+			row.push(null);
+		}
+		
+		board.push(row);
+	}
 
-printBoard(board);
+	let numberOfBombsPlaced = 0;
+
+	while(numberOfBombsPlaced < numberOfBombs) {
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
+	}
+
+	return board;
+}
+
+// function to print out the board in a nice format
+const printBoard = board => { 
+	console.log( board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+console.log('Player board: ');
+printBoard(playerBoard);
+
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Bomb board: ');
+printBoard(bombBoard);
